1a84b8b7c6b6060d5157da60649c640e7325376c
Jan Harasym <jharasym@linux.com>
2022-07-18 00:31:40 +0200

    security: Some XSS prevention

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 extern crate rocket;
 
+use rocket::shield::Frame;
+use rocket::shield::XssFilter;
+use rocket::shield::Hsts;
+
 #[rocket::main]
 async fn main() {
     let figment = rocket::Config::figment().merge(("ident", false));
 
     let _ = rocket::custom(figment)
+        .attach(
+        rocket::shield::Shield::default()
+                .enable(Hsts::default())
+                .enable(XssFilter::Enable)
+                .enable(Frame::SameOrigin)
+        )
         .attach(url::backend::stage())
         .launch()
         .await;
